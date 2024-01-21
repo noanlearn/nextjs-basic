1ec052417ac4cdaca39e60bd30eaf29f44fe14ef
noanlearn <noviadryn@gmail.com>
2024-01-21 11:27:10 +0700

    feat: berpindah halaman dengan Link component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,26 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <h1>Welcome, noanlearn</h1>;
+  return (
+    <main>
+      <header>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/blog">Blog</Link>
+          </li>
+          <li>
+            <Link href="/users">Users</Link>
+          </li>
+        </ul>
+      </header>
+      <h1>Welcome, noanlearn</h1>
+    </main>
+  );
 }
