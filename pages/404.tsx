73b57cd6dd09b5557e5d826cca870fbc9f5bc568
noanlearn <noviadryn@gmail.com>
2024-01-21 14:18:34 +0700

    feat: add auto redirection logic to 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
 export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.push("/");
+    }, 3000);
+  }, []);
+
   return (
     <div className="container-notfound">
       <h1 className="title-notfound">Ooooops...</h1>
