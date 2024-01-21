034ed70e81fa811a74064352d29e562543aabb94
noanlearn <noviadryn@gmail.com>
2024-01-21 12:17:30 +0700

    feat: - global css dan module css - add footer component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from "next/link";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,6 +11,7 @@ export default function Home() {
     <main>
       <Header />
       <h1>Welcome, noanlearn</h1>
+      <Footer />
     </main>
   );
 }
