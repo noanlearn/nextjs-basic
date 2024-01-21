0205b42463d8daf55a438cc5e311b436bb675fbe
noanlearn <noviadryn@gmail.com>
2024-01-21 11:42:44 +0700

    feat: membuat custom component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,14 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import Link from "next/link";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main>
-      <header>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/blog">Blog</Link>
-          </li>
-          <li>
-            <Link href="/users">Users</Link>
-          </li>
-        </ul>
-      </header>
+      <Header />
       <h1>Welcome, noanlearn</h1>
     </main>
   );
