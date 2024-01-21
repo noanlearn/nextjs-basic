415abd0d0b47148475ef99a6a65b00f71c80663b
noanlearn <noviadryn@gmail.com>
2024-01-21 14:04:57 +0700

    feat: add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function Custom404() {
+  return (
+    <div className="container-notfound">
+      <h1 className="title-notfound">Ooooops...</h1>
+      <h1 className="title-notfound">Halaman yang Anda cari tidak ditemukan</h1>
+    </div>
+  );
+}
