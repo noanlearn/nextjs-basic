c8760ed9a4685d1dcbd4104ffb25703997e1d00f
noanlearn <noviadryn@gmail.com>
2024-01-21 15:34:02 +0700

    feat: data fetching with getStaticProps

--- pages/users/index.tsx
+++ pages/users/index.tsx
@@ -1,9 +1,33 @@
 import Layout from "@/components/Layout";
 
-export default function Users() {
+interface UsersProps {
+  dataUsers: Array<any>;
+}
+export default function Users(props: UsersProps) {
+  const { dataUsers } = props;
+
+  console.log(dataUsers);
   return (
     <Layout pageTitle="Users Page">
-      <p>Users Page</p>
+      {dataUsers.map((user) => {
+        return (
+          <div key={user.id}>
+            <p>{user.name}</p>
+            <p>{user.email}</p>
+          </div>
+        );
+      })}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dataUsers = await res.json();
+
+  return {
+    props: {
+      dataUsers,
+    },
+  };
 }
